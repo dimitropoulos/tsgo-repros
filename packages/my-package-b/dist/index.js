218ab39154164267b12cac54a2e78a171716c8c0
Max schwenk <maschwenk@gmail.com>
2025-06-17 14:55:15 -0600

    monorepo version

---
+++ packages/my-package-b/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.myFunction = void 0;
+const myFunction = () => {
+    console.log("Hello from my-package!");
+};
+exports.myFunction = myFunction;
